e7cc32e67acb91d964e4a92102168692a09a66d1
kaeljo01@risk.regn.net <kaeljo01@risk.regn.net>
2022-01-28 09:13:52 -0500

    Create AlphabetDemoUsingExtension

---
+++ AlphabetDemoUsingExtension/AlphabetDemoUsingExtension/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using AlphabetDemoUsingExtension.StringExtensions;
+
+namespace AlphabetDemoUsingExtension
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string alphabet = BuildAlphabet();
+
+            // alphabet
+            Console.WriteLine(alphabet);
+
+            // alphabet reversed using extension method
+            Console.WriteLine(alphabet.Reverse());
+
+            // alphabet every other using extension method
+            Console.WriteLine(alphabet.Skip(2));
+
+            // some other examples
+            Console.WriteLine(alphabet.Skip(-2));
+            Console.WriteLine(alphabet.Skip(4));
+        }
+
+        static string BuildAlphabet()
+        {
+            string alphabet = string.Empty;
+            for (char alpha = 'A'; alpha <= 'Z'; alpha++)
+            {
+                alphabet += alpha;
+            }
+
+            return alphabet;
+        }
+    }
+}
---
+++ AlphabetDemoUsingExtension/AlphabetDemoUsingExtension/StringExtensions/StringExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlphabetDemoUsingExtension.StringExtensions
+{
+    public static class StringExtensions
+    {
+        public static string Reverse(this string myString)
+        {
+            return Skip(myString, -1);
+        }
+
+        public static string Skip(this string myString, int nSkip)
+        {
+            if (myString == null) return null;
+
+            if (nSkip == 0) throw new ArgumentOutOfRangeException("nSkip cannot be 0");
+
+            string skippedString = string.Empty;
+            int i = nSkip < 0 ? myString.Length - 1 : 0; 
+            do
+            {
+                skippedString += myString[i];
+                i += nSkip;
+            } while (nSkip < 0 ? i >= 0 : i < myString.Length);
+
+            return skippedString;
+        }
+    }
+}
